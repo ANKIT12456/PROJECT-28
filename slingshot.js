0554a210728fca46023227fa742d8203feb5dec5
ANKIT12456 <75116891+ANKIT12456@users.noreply.github.com>
2021-01-19 19:15:43 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,38 @@
+class slingshot{
+
+    constructor(bodyA,pointB){
+
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            length:10,
+            stiffness:0.004
+        }
+        this.pointB=pointB;
+        this.bodyA=bodyA;
+        this.body=Constraint.create(options);
+        World.add(world,this.body);
+
+        
+    }
+
+    fly(){
+      
+        this.body.bodyA=null
+    }
+
+        attach(body1){
+            this.body.bodyA=body1;
+         }
+
+    display(){
+
+        if(this.body.bodyA){
+        var posa=this.body.bodyA.position;
+        var posb=this.pointB;
+
+        strokeWeight(2);
+        line (posa.x,posa.y,posb.x,posb.y);
+        }
+    }
+}
